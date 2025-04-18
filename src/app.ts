cbc698985a1217975fb1d3847cd8530e8ae9cf6b
viitones <vitonesmartins@gmail.com>
2025-04-18 19:31:13 -0300

    :sparkles: Handler de erros global

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,26 @@
 import fastify from 'fastify';
+import { ZodError } from 'zod';
+import { env } from './env';
 import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((err, _, reply) => {
+  if (err instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: err.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(err);
+  } else {
+    // Log the error to an external service
+    // e.g., Sentry, Loggly, etc.
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' });
+});
