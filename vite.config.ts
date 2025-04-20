1065bb4efdd28a95aee6eae143c2def4919e78cb
viitones <vitonesmartins@gmail.com>
2025-04-19 22:19:45 -0300

    :wrench: add vitest coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      include: ['src/**/*.{ts,tsx}'],
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: 'coverage',
+    },
+  },
 });
