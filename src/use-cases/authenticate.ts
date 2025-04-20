d7f7c2fc8316f1bfa8861a8b23deee4f3d3c508b
viitones <vitonesmartins@gmail.com>
2025-04-19 22:40:19 -0300

    :sparkles: Caso de uso de autenticação

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,37 @@
+import { compare } from 'bcryptjs';
+import { User } from 'generated/prisma/client';
+import { InvalidCredentialsError } from './errors/invalid-credentials-error';
+import { UsersRepository } from '@/repositories/users-repository';
+
+interface AuthenticateUseCaseRequest {
+  email: string;
+  password: string;
+}
+interface AuthenticateUseCaseResponse {
+  user: User;
+}
+
+export class AuthenticateUseCase {
+  constructor(private userRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
